f729c737f43f7463b05bdac28bc1e843667eef43
Fernanda Alcantara <fernanda.alcantara2407@gmail.com>
2024-09-11 17:25:59 -0300

    check

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,18 @@
+using System.Diagnostics;
+
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        
+        public Nokia(string numero) : base(numero)
+        {
+
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+             Console.WriteLine(nomeApp);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia n1 = new Nokia("(22) 9 7874-8589");
+n1.Ligar();
+n1.ReceberLigacao();
+n1.InstalarAplicativo("Pou");
+
+Iphone i1 = new Iphone ("(21) 9 2474-3343");
+i1.Ligar();
+i1.ReceberLigacao();
+i1.InstalarAplicativo("caféLand");
